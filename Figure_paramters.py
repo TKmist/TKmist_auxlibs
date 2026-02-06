5ec68ac1ff772c024bee6b6be4a53bf008880573
TKmist <tkalwarczyk@ichf.edu.pl>
2026-02-06 23:18:57 +0100

    zmieniono:       Figure_paramters.py

--- Figure_paramters.py
+++ Figure_paramters.py
@@ -5,7 +5,7 @@ import importlib.metadata as im
 
 
 class Figure_data:
-    def __init__(self,manuscript_path,outfile):
+    def __init__(self,manuscript_path,outfile,packages=None):
         self.label_font = 20
         self.ticlabelfont = 15
         self.legend_font_size = 10
@@ -24,39 +24,31 @@ class Figure_data:
         self.ls_loosely_dashdotdotted = (0, (5, 4, 1, 4, 1, 4))
         
         self.outfile = outfile
-        self.save_env_versions(self.outfile)
+        self.packages = packages or []
+        self.save_env_versions(self.outfile, self.packages)
 
-    def save_env_versions(self, outfile="env_versions.txt"):
-        used_packages = {}
-    
-        modules_snapshot = list(sys.modules.values())
-        for module in modules_snapshot:
-            name = getattr(module, "__package__", None) or getattr(module, "__name__", None)
-            if not name:
-                continue
-    
-            root = name.split(".")[0]
-            if root in used_packages:
-                continue
-    
-            try:
-                used_packages[root] = im.version(root)
-            except im.PackageNotFoundError:
-                pass
-    
+    def save_env_versions(self, outfile="env_versions.txt", packages=None):
+        packages = packages or []
+
         lines = [f"Python {sys.version.split()[0]}", ""]
-        for pkg in sorted(used_packages):
-            lines.append(f"{pkg}=={used_packages[pkg]}")
-    
+
+        for pkg in packages:
+            try:
+                ver = im.version(pkg)
+                lines.append(f"{pkg}=={ver}")
+            except im.PackageNotFoundError:
+                lines.append(f"{pkg}==NOT_INSTALLED")
+
         Path(outfile).write_text("\n".join(lines))
         
     def convert_pdf_to_eps(self,file_path):
+        print("Converting PDF to EPS:")
         try:
             result = subprocess.run(['pdftops', '-eps', file_path], check=True, capture_output=True, text=True)
-            print("Command succeeded")
+            print("Convertion succeeded")
             print(result.stdout)
         except subprocess.CalledProcessError as e:
-            print("Command failed")
+            print("Convertion failed")
             print(e.stderr)
 
 
